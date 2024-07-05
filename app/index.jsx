076796ca6304df08e22d2ed7e3efc730ae811fca
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-07-06 00:35:30 +0530

    update loading screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Hello, praveen</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className="text-blue-500">
-       Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <View className="w-full flex-row justify-center items-center">
+            <Image
+              source={images.logo}
+              className="w-[130px] h-[84px]"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text
+              className="text-3xl text-white
+            font-bold text-center"
+            >
+              Discover Endless Possibilities with {""}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 - right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text>
+            Where creativity meets innovation : embark on a journey of limits
+            exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
